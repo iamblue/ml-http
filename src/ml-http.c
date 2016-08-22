867fca3eac1486472589f178089d2c7f4bc4ddb4
po-ju.chen <tonyone0902@gmail.com>
2016-08-22 14:39:44 +0800

    upgrade to new jerry api

--- src/ml-http.c
+++ src/ml-http.c
@@ -1,86 +1,80 @@
 #include <string.h>
-#include "jerry.h"
+#include "jerry-api.h"
 #include "httpclient.h"
 #include "microlattice.h"
 
 DELCARE_HANDLER(__http) {
-  if (args_cnt == 2 && args_p[0].type == JERRY_API_DATA_TYPE_OBJECT) {
-    jerry_api_value_t method;
-    jerry_api_value_t header;
-    jerry_api_value_t url;
+  if (args_cnt == 2 && jerry_value_is_object(args_p[0])) {
 
-    // method
-    jerry_api_get_object_field_value (args_p[0].v_object, (jerry_api_char_t *) "method", &method);
-    // url
-    jerry_api_get_object_field_value (args_p[0].v_object, (jerry_api_char_t *) "url", &url);
+    jerry_value_t prop_method = jerry_create_string ((const jerry_char_t *) "method");
+    jerry_value_t method = jerry_get_property(args_p[0], prop_method);
 
-    /* method */
-    int method_req_sz = -jerry_api_string_to_char_buffer(method.v_string, NULL, 0);
-    char * method_buffer = (char*) malloc (method_req_sz);
-    method_req_sz = jerry_api_string_to_char_buffer (method.v_string, (jerry_api_char_t *) method_buffer, method_req_sz);
-    method_buffer[method_req_sz] = '\0';
+    jerry_value_t prop_url = jerry_create_string ((const jerry_char_t *) "url");
+    jerry_value_t url = jerry_get_property(args_p[0], prop_url);
 
-    /* url */
-    int url_req_sz = -jerry_api_string_to_char_buffer (url.v_string, NULL, 0);
-    char * url_buffer = (char*) malloc (url_req_sz);
-    url_req_sz = jerry_api_string_to_char_buffer (url.v_string, url_buffer, url_req_sz);
+    jerry_value_t prop_header = jerry_create_string ((const jerry_char_t *) "header");
+    jerry_value_t header = jerry_get_property(args_p[0], prop_header);
+
+    jerry_size_t url_req_sz = jerry_get_string_size (url);
+    jerry_char_t url_buffer[url_req_sz];
+    jerry_string_to_char_buffer (url, url_buffer, url_req_sz);
     url_buffer[url_req_sz] = '\0';
+
+    jerry_size_t header_req_sz = jerry_get_string_size (header);
+    jerry_char_t header_buffer[header_req_sz];
+    jerry_string_to_char_buffer (header, header_buffer, header_req_sz);
+    header_buffer[header_req_sz] = '\0';
 
     httpclient_t client = {0};
     httpclient_data_t client_data = {0};
 
-    // char *buf;
-    // buf = pvPortMalloc(BUF_SIZE);
-    // if (buf == NULL) {
-    //     printf("httpclient_test malloc failed.\r\n");
-    //     return true;
-    // }
-
-    char buf[100] = {0};
+    char *buf = (char*) malloc (1024);
     client_data.response_buf = buf;
-    client_data.response_buf_len = 100;
+    client_data.response_buf_len = 1024;
 
-    // header
-    bool isHeader = jerry_api_get_object_field_value (args_p[0].v_object, (jerry_api_char_t *) "header", &header);
-    int header_req_sz = 0;
-    char * header_buffer =(char*) malloc (header_req_sz);
     /* header */
-    if (isHeader) {
-      header_req_sz = -jerry_api_string_to_char_buffer (header.v_string, NULL, 0);
-      header_buffer = (char*) malloc (header_req_sz);
-      header_req_sz = jerry_api_string_to_char_buffer (header.v_string, header_buffer, header_req_sz);
-      header_buffer[header_req_sz] = '\0';
+    if (!jerry_value_has_error_flag(header)) {
       httpclient_set_custom_header(&client, header_buffer);
     }
 
-    if (strncmp (method_buffer, "POST", (size_t)method_req_sz) == 0) {
-      jerry_api_value_t data;
-      jerry_api_value_t contentType;
+    /* hint !!! method_buffer must be here */
+    jerry_size_t method_req_sz = jerry_get_string_size (method);
+    jerry_char_t method_buffer[method_req_sz];
+    jerry_string_to_char_buffer (method, method_buffer, method_req_sz);
+    method_buffer[method_req_sz] = '\0';
 
+    if (strncmp (method_buffer, "POST", (size_t)method_req_sz) == 0) {
       // data
-      jerry_api_get_object_field_value (args_p[0].v_object, (jerry_api_char_t *) "data", &data);
+      jerry_value_t prop_data = jerry_create_string ((const jerry_char_t *) "data");
+      jerry_value_t data = jerry_get_property(args_p[0], prop_data);
+
       // contentType
-      jerry_api_get_object_field_value (args_p[0].v_object, (jerry_api_char_t *) "contentType", &contentType);
+      jerry_value_t prop_contentType = jerry_create_string ((const jerry_char_t *) "contentType");
+      jerry_value_t contentType = jerry_get_property(args_p[0], prop_contentType);
 
       /* data */
-      int data_req_sz = -jerry_api_string_to_char_buffer (data.v_string, NULL, 0);
-      char * data_buffer = (char*) malloc (data_req_sz);
-      data_req_sz = jerry_api_string_to_char_buffer (data.v_string, data_buffer, data_req_sz);
+      jerry_size_t data_req_sz = jerry_get_string_size (data);
+      jerry_char_t data_buffer[data_req_sz];
+      jerry_string_to_char_buffer (data, data_buffer, data_req_sz);
       data_buffer[data_req_sz] = '\0';
 
       /* contentType */
-      int contentType_req_sz = -jerry_api_string_to_char_buffer (contentType.v_string, NULL, 0);
-      char * contentType_buffer = (char*) malloc (contentType_req_sz);
-      contentType_req_sz = jerry_api_string_to_char_buffer (contentType.v_string, contentType_buffer, contentType_req_sz);
+      jerry_size_t contentType_req_sz = jerry_get_string_size (contentType);
+      jerry_char_t contentType_buffer[contentType_req_sz];
+      jerry_string_to_char_buffer (contentType, contentType_buffer, contentType_req_sz);
       contentType_buffer[contentType_req_sz] = '\0';
 
       client_data.post_content_type = contentType_buffer;
       client_data.post_buf = data_buffer;
-      client_data.post_buf_len = strlen(data_buffer);
+      client_data.post_buf_len = data_req_sz;
 
       httpclient_post(&client, url_buffer, HTTP_PORT, &client_data);
-      jerry_api_release_object(&data);
-      jerry_api_release_object(&contentType);
+
+      jerry_release_value(prop_data);
+      jerry_release_value(prop_contentType);
+      jerry_release_value(data);
+      jerry_release_value(contentType);
+
       free(data_buffer);
       free(contentType_buffer);
 
@@ -88,24 +82,32 @@ DELCARE_HANDLER(__http) {
       httpclient_get(&client, url_buffer, HTTP_PORT, &client_data);
     }
 
-    jerry_api_value_t params[0];
-    params[0].type = JERRY_API_DATA_TYPE_STRING;
-    params[0].v_string = jerry_api_create_string (client_data.response_buf);
-    jerry_api_call_function(args_p[1].v_object, NULL, false, &params, 1);
-    jerry_api_release_value(&params);
+    jerry_value_t params[0];
+    params[0] = jerry_create_string(client_data.response_buf);
+
+    jerry_value_t this_val = jerry_create_undefined();
+    jerry_value_t ret_val = jerry_call_function (args_p[1], this_val, &params, 1);
+
+    jerry_release_value(params);
+    jerry_release_value(this_val);
+    jerry_release_value(ret_val);
 
     httpclient_close(&client, HTTP_PORT);
-    ret_val_p->type = JERRY_API_DATA_TYPE_BOOLEAN;
-    ret_val_p->v_bool = true;
 
-    jerry_api_release_object(&header);
-    jerry_api_release_object(&method);
-    jerry_api_release_object(&url);
+    jerry_release_value(header);
+    jerry_release_value(method);
+    jerry_release_value(url);
+    jerry_release_value(prop_header);
+    jerry_release_value(prop_method);
+    jerry_release_value(prop_url);
+
+    free(buf);
     free(method_buffer);
     free(url_buffer);
     free(header_buffer);
-    return true;
+    buf = NULL;
   }
+  return jerry_create_boolean (true);
 }
 
 
